f69e4367f0ee310a3c892687f705e932b9fa128b
Tarikuzuma <edwinjrgumba@gmail.com>
2025-01-07 02:29:01 +0800

    prob: added 28

---
+++ 28-FindTheIndexoftheFirstOccurance.py
@@ -0,0 +1,20 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        else:
+            return 0 if len(needle) == 0 else haystack.index(needle)
+        
+def TestSolution():
+    assert Solution().strStr("hello", "ll") == 2, "Test case 1 failed"
+    assert Solution().strStr("aaaaa", "bba") == -1, "Test case 2 failed"
+    
+    print("All test cases pass")
+
+if __name__ == "__main__":
+    TestSolution() 
+    # solution = Solution()
+    # string = "czuu"
+    # queues = [[0, 0, 0], [1, 1, 1]]
+    # solution.shiftingLetters(string, queues)
+        
