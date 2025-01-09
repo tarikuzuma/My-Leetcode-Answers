6be2267c497ab55624863f994ee98ecf06734642
Tarikuzuma <edwinjrgumba@gmail.com>
2025-01-10 01:10:26 +0800

    lang: added java implementation

--- 2185-CountingWordsWithAGiven.py
+++ 2185-CountingWordsWithAGiven.py
@@ -23,3 +23,22 @@ def TestSolution():
 
 if __name__ == "__main__":
     TestSolution()
+
+'''
+Java:
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+'''
